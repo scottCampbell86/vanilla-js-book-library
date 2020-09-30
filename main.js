a3c6e8ca06b6849bf50a089a344e966736491102
Scott Campbell <stc9486@gmail.com>
2020-09-30 19:02:00 -0400

    validate

--- main.js
+++ main.js
@@ -12,6 +12,30 @@ function Book(title, author, isbn) {
 //UI constructor
 function UI() {};
 
+//show alert
+
+UI.prototype.showAlert = (message, className) => {
+  const div = createE('div');
+  div.className = `alert ${className}`;
+
+  div.appendChild(document.createTextNode(message));
+
+  const containter = qs('.container');
+  const form = qs('#book-form');
+
+  containter.insertBefore(div, form);
+
+  setTimeout(() => {
+    qs('.alert').remove();
+  }, 2500);
+}
+//UI method to clear input fields
+UI.prototype.clearFields = () => {
+  qs('#title').value = '';
+  qs('#author').value = '';
+  qs('#isbn').value = '';
+}
+
 //UI class method to add book to list
 UI.prototype.addBookToList = book => {
   //create var for book list
@@ -48,8 +72,16 @@ document.getElementById('book-form').addEventListener('submit', (e) => {
   //Instantiate instance of UI
   const ui = new UI();
 
-  //Add book to list
-  ui.addBookToList(book);
+  //validate
+  if (title === '' || author === '' || isbn === '') ui.showAlert('Please fill in all fields', 'error');
+  else {
+    //Add book to list
+    ui.addBookToList(book);
+    //alert success
+    ui.showAlert('Book added', 'success')
+    //clear field
+    ui.clearFields()
+  }
 
 })
 

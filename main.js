407290d8f2c595f1d12d62ebe96bc2404402c003
Scott Campbell <stc9486@gmail.com>
2020-10-01 17:54:04 -0400

    done

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-//helpers
+//Helpers
 const qs = (input) => document.querySelector(input);
 const createE = (input) => document.createElement(input);
 
-//Book constructor
+//Book Constructor
 function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
   this.isbn = isbn;
 }
 
-//UI constructor
+//UI Constructor
 function UI() {};
 
-//show alert
 
+  //show alert
 UI.prototype.showAlert = (message, className) => {
   const div = createE('div');
   div.className = `alert ${className}`;
@@ -30,23 +30,21 @@ UI.prototype.showAlert = (message, className) => {
   }, 2250);
 }
 
-//delete book
 
+  //delete book
 UI.prototype.deleteBook = target => target.className === 'delete' && target.parentElement.parentElement.remove();
 
-//UI method to clear input fields
+  //UI method to clear input fields
 UI.prototype.clearFields = () => {
   qs('#title').value = '';
   qs('#author').value = '';
   qs('#isbn').value = '';
 }
 
-//UI class method to add book to list
+  //UI class method to add book to list
 UI.prototype.addBookToList = book => {
-  //create var for book list
-  const list = qs('#book-list');
 
-  //create tr element
+  const list = qs('#book-list');
   const row = createE('tr');
 
   row.innerHTML = 
@@ -64,31 +62,32 @@ UI.prototype.addBookToList = book => {
 document.getElementById('book-form').addEventListener('submit', (e) => {
   e.preventDefault();
 
-  //grab form values
+    //grab form values
   const title = qs('#title').value;
         author = qs('#author').value;
         isbn = qs('#isbn').value;
 
-  console.log(title, author, isbn);
-
-  //Instantiate new Book class
+    //instantiate new Book class
   const book = new Book(title, author, isbn);
 
-  //Instantiate instance of UI
+    //instantiate instance of UI
   const ui = new UI();
 
-  //validate
+    //validate
+      //if fails
   if (title === '' || author === '' || isbn === '') ui.showAlert('Please fill in all fields', 'error');
+      //if passes
   else {
-    //Add book to list
+      //add book to list
     ui.addBookToList(book);
-    //alert success
+      //alert success
     ui.showAlert('Book added', 'success')
-    //clear field
+      //clear field
     ui.clearFields()
   }
 })
 
+//remove book from DOM
 qs('#book-list').addEventListener('click', (e) => {
   e.preventDefault();
 
